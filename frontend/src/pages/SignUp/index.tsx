20d4b9f55c5cd31f7ba8535f6173982a34d8f214
thauam09 <thauam_09martins@hotmail.com>
2021-02-12 00:09:46 -0300

    created signup page

---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+        <Input name="senha" icon={FiLock} placeholder="Senha" type="password" />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+      <a href="signup">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
