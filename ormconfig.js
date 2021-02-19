f30ff2608483d98af4bcf5473b7b05e353740ae7
thauam09 <thauam_09martins@hotmail.com>
2021-02-19 09:34:05 -0300

    created .env file and  db variables

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'gostack_gobarber',
+  entities: ['./src/models/*.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+};
