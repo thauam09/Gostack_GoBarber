317b20c7f1108c3dd71f7262dca62693ac861bfc
thauam09 <thauam_09martins@hotmail.com>
2021-02-19 01:14:48 -0300

    changed file directory

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
-app.get('/', (req, res) => {
-  res.send('This is a sample express app');
-});
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
